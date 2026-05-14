c66164dd4d5048196cd7c791b85ade2cd627c79f
Juan Villela <fourjuaneight@users.noreply.github.com>
2026-05-14 19:15:49 -0400

    fix: Remove dead code on yt-dlp script.

--- utils/ytdl.go
+++ utils/ytdl.go
@@ -1,37 +1,11 @@
 package utils
 
-import (
-	"fmt"
-	"strings"
-)
-
-func installBin() error {
-	wgetErr := CMD("sudo", "wget", "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp", "-O", "/usr/local/bin/yt-dlp")
-	if wgetErr != nil {
-		return fmt.Errorf("[installBin][wget]: %w", wgetErr)
-	}
-
-	chmodErr := CMD("sudo", "chmod", "a+rx", "/usr/local/bin/yt-dlp")
-	if chmodErr != nil {
-		return fmt.Errorf("[installBin][chmod]: %w", chmodErr)
-	}
-
-	return nil
-}
+import "fmt"
 
 // Download YT video from url.
 func YTDL(url string, name string) error {
-	err := CMD("yt-dlp", "-f", "b", "--merge-output-format", "mp4", "-o", name, url)
-	if err != nil {
-		if strings.Contains(err.Error(), "no such file or directory") {
-			err := installBin()
-			if err != nil {
-				return fmt.Errorf("[YTDL]: %w", err)
-			}
-		}
-
+	if err := CMD("yt-dlp", "-f", "b", "--merge-output-format", "mp4", "-o", name, url); err != nil {
 		return fmt.Errorf("[YTDL][yt-dlp]: %w", err)
 	}
-
 	return nil
 }
